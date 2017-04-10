bc7862be373c451b5d9c8d1846aa2463e90b560a
kpodranyuk <katie.katie2011@yandex.ru>
2017-04-10 21:32:42 +0300

    Добавлены структуры выражения и списка выражений.

--- PythonCompiler/tree_structs.h
+++ PythonCompiler/tree_structs.h
@@ -24,3 +24,35 @@ enum ExprType
 	_OPERAND,
 	_FUNCCALL
 };
+/*
+* Хранит в себе информацию об узле типа выражения
+*/
+struct ExprInfo
+{
+	// Левый операнд выражения
+	struct ExprInfo * left;
+	// Правый операнд выражения
+	struct ExprInfo * right;
+	// Указатель на следующий элемент (выражение)
+	struct ExprInfo * next;
+	// Указатель на список аргументов выражения
+	struct ExprListInfo * arglist;
+	// Тип выражения
+	enum ExprType type;
+
+	// Значение выражения-строки
+	char * opVal;
+	// Значение выражения-целого числа
+	int intVal;
+	// Значение выражения-логической переменной
+	bool logVal;
+};
+/*
+* Хранит в себе указатель на начало и конец списка выражений
+* (возможно следует изменить тип - хранить начало и следующий)
+*/
+struct ExprListInfo
+{
+	struct ExprInfo * first;
+	struct ExprInfo * last;
+};
