7006b8a89e1790faf98ce72315c2be58c8fb8fa6
kpodranyuk <katie.katie2011@yandex.ru>
2017-04-11 21:21:15 +0300

    Заголовки функций печати вынесены в начало файла. Оговорены формы узлов дерева в зависимости от их типа.

--- PythonCompiler/tree_print.h
+++ PythonCompiler/tree_print.h
@@ -9,6 +9,16 @@
 * Пример: 1 -- 2;
 **/
 
+/*---------	ОПЕРАЦИИ - КВАДРАТНЫЕ ФОРМЫ УЗЛОВ, ОПЕРАНДЫ - КРУГЛЫЕ (по умолчанию без тега shape)	---------*/
+
+void printExpr(struct ExprInfo * expr, int* nodeCount, std::vector<std::string>& dotTree);
+void printIfStmt(struct IfStmtInfo * ifstmt, int* nodeCount, std::vector<std::string>& dotTree);
+void printWhileStmt(struct WhileStmtInfo * whilestmt, int* nodeCount, std::vector<std::string>& dotTree);
+void printForStmt(struct ForStmtInfo * forstmt, int* nodeCount, std::vector<std::string>& dotTree);
+void printFuncDefStmt(struct FuncDefStmtInfo * funcdefstmt, int* nodeCount, std::vector<std::string>& dotTree);
+void printContinueBreakStmt(int* nodeCount, std::vector<std::string>& dotTree);
+void printReturnStmt(struct ExprInfo * expr, int* nodeCount, std::vector<std::string>& dotTree);
+void printDelStmt(struct ExprInfo * expr, int* nodeCount, std::vector<std::string>& dotTree);
 
 /* Обойти дерево и "перевести" для печати на язык dot
 * \param[in] root список корней дерева
