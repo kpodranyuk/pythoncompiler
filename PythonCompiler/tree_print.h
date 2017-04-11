7b831c3e9d7fe4a81747c2e354965f9ad9ef2b4d
nbiryukov <nbiryukov96@gmail.com>
2017-04-11 11:53:13 +0400

    Добавлены функци печати стейтментов.

--- PythonCompiler/tree_print.h
+++ PythonCompiler/tree_print.h
@@ -38,3 +38,36 @@ void printStatementList(struct StmtListInfo* root, int* nodeCount, std::vector<s
 		begining = begining->next;
 	}
 }
+
+
+void printExpr(struct ExprInfo * expr, int* nodeCount, std::vector<std::string>& dotTree)
+{
+}
+
+void printIfStmt(struct IfStmtInfo * ifstmt, int* nodeCount, std::vector<std::string>& dotTree)
+{
+}
+
+void printWhileStmt(struct WhileStmtInfo * whilestmt, int* nodeCount, std::vector<std::string>& dotTree)
+{
+}
+
+void printForStmt(struct ForStmtInfo * forstmt, int* nodeCount, std::vector<std::string>& dotTree)
+{
+}
+
+void printFuncDefStmt(struct FuncDefStmtInfo * funcdefstmt, int* nodeCount, std::vector<std::string>& dotTree)
+{
+}
+
+void printContinueBreakStmt(int* nodeCount, std::vector<std::string>& dotTree)
+{
+}
+
+void printReturnStmt(struct ExprInfo * expr, int* nodeCount, std::vector<std::string>& dotTree)
+{
+}
+
+void printDelStmt(struct ExprInfo * expr, int* nodeCount, std::vector<std::string>& dotTree)
+{
+}
