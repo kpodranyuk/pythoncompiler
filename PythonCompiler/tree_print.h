b082d28abee759fec6374db7f79e58a9724f4424
kpodranyuk <katie.katie2011@yandex.ru>
2017-04-11 19:13:51 +0300

    Добавлены вызовы функций в главной функции обхода дерева. Добавлена краткая справка по языку dot.

--- PythonCompiler/tree_print.h
+++ PythonCompiler/tree_print.h
@@ -1,40 +1,70 @@
 #include "tree_structs.h"
 #include <vector>
 
+/** КРАТКАЯ СПРАВКА ПО ЯЗЫКУ DOT
+* Сам граф должен объявляться как: graph <Имя> {тело графа}
+* Объявление узла: <номер узла> [label=\"строка с текстом в узле\" shape=форма узла];
+* Пример: 1 [label=\"{4}\" shape=box];
+* Связи между узлами (если граф не направленный): <номер узла> -- <номер узла>;
+* Пример: 1 -- 2;
+**/
+
+
+/* Обойти дерево и "перевести" для печати на язык dot
+* \param[in] root список корней дерева
+* \param[in|out] nodeCount текущий индекс узла
+* \param[in|out] dotTree список строк для хранения кода на dot
+*/
 void printStatementList(struct StmtListInfo* root, int* nodeCount, std::vector<std::string>& dotTree)
 {
-	struct StmtInfo* begining;
+	// Если список корней дерева null - выходим из функции
 	if(root==NULL) return ;
+	// Иначе
+	// Создаем локальный указатель на элемент входного дерева
+	struct StmtInfo* begining;
+	// Считаем первый элемент списка начальным
 	begining = root->first;
+	// Пока текущий элемент списка не последний..
 	while(begining!=NULL)
 	{
+		// В зависимости от типа узла/элемента вызываем соответствующую функцию
 		if(begining->type==_EXPR)
 		{
+			printExpr(begining->expr,nodeCount,dotTree);
 		}
 		else if(begining->type==_IF)
 		{
+			printIfStmt(begining->ifstmt,nodeCount,dotTree);
 		}
 		else if(begining->type==_FOR)
 		{
+			printForStmt(begining->forstmt,nodeCount,dotTree);
 		}
 		else if(begining->type==_WHILE)
 		{
+			printWhileStmt(begining->whilestmt,nodeCount,dotTree);
 		}
 		else if(begining->type==_FUNC_DEF)
 		{
+			printFuncDefStmt(begining->funcdefstmt,nodeCount,dotTree);
 		}
 		else if(begining->type==_RETURN)
 		{
+			printReturnStmt(begining->expr,nodeCount,dotTree);
 		}
 		else if(begining->type==_BREAK)
 		{
+			printContinueBreakStmt(nodeCount,dotTree);
 		}
 		else if(begining->type==_CONTINUE)
 		{
+			printContinueBreakStmt(nodeCount,dotTree);
 		}
 		else if(begining->type==_DEL)
 		{
+			printDelStmt(begining->expr,nodeCount,dotTree);
 		}
+		// Считаем следующий элемент списка новым текущим
 		begining = begining->next;
 	}
 }
