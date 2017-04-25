95dd19cc4eae79fb3be578489bcc056f07ff3425
nbiryukov <nbiryukov96@gmail.com>
2017-04-25 08:41:44 +0400

    Исправлен входной тестовый файл со всеми операциями. Убраны лишние операции.

--- PythonCompiler/test.py
+++ PythonCompiler/test.py
@@ -35,16 +35,8 @@ print(list2[-1])
 
 def funcOperation(a, b): #Comment
     a = b
-    a %= 1
-    a //= 1
-    a /= 5
-    a -=0
-    a += 10
-    a *= 2
-    a **= 1
     a * b
     a ** b
-    b % 2
     c = a / 10
     c = a // 5
     c = a + 4
