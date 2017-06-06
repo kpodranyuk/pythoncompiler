959b861798dce133f48602dc0f48b886ce310f40
kpodranyuk <katie.katie2011@yandex.ru>
2017-06-06 09:29:00 +0300

    Добавлены структуры таблиц.

--- PythonCompiler/tables_structs.h
+++ PythonCompiler/tables_structs.h
@@ -22,7 +22,7 @@ enum ConstType
 */
 struct ConstTable_Elem
 {
-    enum ConstType type;	// Тип константы
+    enum ConstType type;	// Тип константы - u1
 	/*
 	* Хранит в себе значение константы
 	*/
@@ -42,4 +42,42 @@ struct ConstTable_Elem
     } value;
     
     struct ConstTable_Elem * next;	// Указатель на следующий элемент таблицы
+};
+
+/*
+* Хранит в себе информацию об элементе таблицы полей
+*/
+struct FieldTable_Elem
+{
+	int fieldName;	// Ссылка на имя поля в таблице констант
+	int fieldDesc;	// Ссылка на дескриптор поля в таблице константс
+
+	struct FieldTable_Elem* next;	// Указатель на следующий элемент таблицы
+};
+
+/*
+* Хранит в себе информацию об элементе таблицы методов
+*/
+struct MethodTable_Elem
+{
+	int methodName;	// Ссылка на имя поля в таблице констант
+	int methodDesc;	// Ссылка на дескриптор поля в таблице константс
+	const int attrs = 1;	// Количество аттрибутов метода
+	//TODO сделать ссылку на таблицу аттрибутов метода
+	struct CodeAttr* methodAttr;
+	struct MethodTable_Elem* next;	// Указатель на следующий элемент таблицы
+};
+
+/*
+* Хранит в себе информацию об аттрибуте кода метода
+*/
+struct CodeAttr
+{
+	int attrName;	// Ссылка на аттрибут Code в таблице констант
+	int attrLength;	// Длина атрибута (кроме первых 6 байт, описывающих имя и длину)
+	const int stackSize = 1200;	// Размер стека операндов
+	int localVarsCount;	// Количество локальных переменных метода
+	int byteCodeLength;	// Длина байт-кода (размер кода метода в байтах)
+	const int exceptionsCount = 0;	// Количество записей в таблице исключений
+	const int attrsCount = 0;		// Количество аттрибутов
 };
